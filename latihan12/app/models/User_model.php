42fc76da4e2cac48524075040930af53daf7d09c
Mbah Syahrul <syahrulimron111@gmail.com>
2021-10-24 06:28:52 +0700

    PHP
    MVC PHP

---
+++ latihan12/app/models/User_model.php
@@ -0,0 +1,12 @@
+<?php 
+
+    class User_model {
+        private $nama = 'Mas Syahrul MSI';
+
+        public function getUser()
+        {
+            return $this->nama;
+        }
+    }
+
+?>
